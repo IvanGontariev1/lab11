4b805842995d59da6c7f1263b448d4e699d3be54
grush <grush@DESKTOP-JFK81GP>
2024-06-25 23:54:59 +0300

    Добавьте файлы проекта.

---
+++ lab11/lab11.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main() {
+
+    double a, b;
+    std::cout << "Enter the value for a: ";
+    std::cin >> a;
+    std::cout << "Enter the value for b: ";
+    std::cin >> b;
+
+
+    double* pA = &a;
+    double* pB = &b;
+
+
+    if (*pA > *pB) {
+        *pA += 3;
+        *pB /= 3;
+    }
+    else {
+        *pA /= 2;
+        *pB += 3;
+    }
+
+
+    std::cout << "Updated value of a: " << a << std::endl;
+    std::cout << "Updated value of b: " << b << std::endl;
+
+    return 0;
+}
